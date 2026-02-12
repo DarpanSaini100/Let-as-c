8934154f59ceaa3ef4e6b4b4b7eeef40e7863b67
Darpan Saini <darpansaini19978@gmail.com>
2026-02-12 21:28:35 +0530

    update

--- c_code/add.c
+++ c_code/add.c
@@ -2,11 +2,11 @@
 int main()
 {
    int a , b , sum ;
-    printf("enter first number :");
+    printf("enter first number :\n");
     scanf("%d",&a);
-    printf("enter second number :");
+    printf("enter second number :\n");
     scanf("%d",&b);
     sum = a + b ;
-    printf("sum of %d and %d is %d",a,b,sum);
+    printf("sum of %d and %d is %d\n",a,b,sum);
     return 0;
 }
