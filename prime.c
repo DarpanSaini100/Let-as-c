53734a59daa8195c07e584280ede59546f1cfd5d
Darpan Saini <darpansaini19978@gmail.com>
2026-02-18 16:18:18 +0530

    update

--- prime.c
+++ prime.c
@@ -6,12 +6,12 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // 0 and 1 are not prime numbers
+    
     if (num <= 1) {
         isPrime = 0;
     }
 
-    // Check divisibility
+
     for(i = 2; i <= num/2; i++) {
         if(num % i == 0) {
             isPrime = 0;
